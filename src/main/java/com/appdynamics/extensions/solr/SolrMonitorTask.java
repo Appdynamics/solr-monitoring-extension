ce48feab7035ce23ceaf63b44f89a6863651a2ef
adityajagtiani89 <adityajagtiani@ajagtiani-mac.corp.appdynamics.com>
2016-10-31 15:26:04 -0700

    Minor change

--- src/main/java/com/appdynamics/extensions/solr/SolrMonitorTask.java
+++ src/main/java/com/appdynamics/extensions/solr/SolrMonitorTask.java
@@ -235,7 +235,7 @@ public class SolrMonitorTask implements Runnable {
      * @param metricValue
      */
     private void printMetric (String metricPath, String metricName, Object metricValue) {
-        printMetric(configuration.getMetricPrefix() + "|" + server.get("name") + metricPath, metricName, metricValue, MetricWriter
+        printMetric(configuration.getMetricPrefix() + METRIC_SEPARATOR + server.get("name") + metricPath, metricName, metricValue, MetricWriter
                 .METRIC_AGGREGATION_TYPE_AVERAGE, MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE, MetricWriter
                 .METRIC_CLUSTER_ROLLUP_TYPE_INDIVIDUAL);
     }
