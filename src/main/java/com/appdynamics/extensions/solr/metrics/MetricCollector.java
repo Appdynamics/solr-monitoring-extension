59f19a0beacb8959bdc10ca7141afaca9f628e16
Bhuvnesh Kumar <raghur171@gmail.com>
2018-05-23 16:22:21 -0700

    updated copyright

--- src/main/java/com/appdynamics/extensions/solr/metrics/MetricCollector.java
+++ src/main/java/com/appdynamics/extensions/solr/metrics/MetricCollector.java
@@ -1,3 +1,11 @@
+/*
+ *   Copyright 2018 . AppDynamics LLC and its affiliates.
+ *   All Rights Reserved.
+ *   This is unpublished proprietary source code of AppDynamics LLC and its affiliates.
+ *   The copyright notice above does not evidence any actual or intended publication of such source code.
+ *
+ */
+
 package com.appdynamics.extensions.solr.metrics;
 /**
  * Created by bhuvnesh.kumar on 4/10/18.
